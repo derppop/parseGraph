6afc91606128c2eec1b8e3a02c90327808093b0d
derppop <yortbo@gmail.com>
2023-09-20 18:34:30 -0500

    added NetModelGenerator.jar dependency and starting to generate/perturb graph all within this project

--- build.sbt
+++ build.sbt
@@ -1,9 +1,11 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "2.13.12"
+ThisBuild / scalaVersion := "3.2.2"
 
 lazy val root = (project in file("."))
   .settings(
     name := "parseGraph"
   )
 mainClass in Compile := Some("Main")
+
+unmanagedBase := baseDirectory.value / "src" / "main" / "resources" / "lib"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,33 +1,9 @@
+import NetGraphAlgebraDefs.{NetGraph, NetModel, NetModelAlgebra}
+import NetGraphAlgebraDefs.NetModelAlgebra.outputDirectory
+import Utilz.NGSConstants
 object Main{
   def main(args: Array[String]): Unit = {
-    if (args.length > 0) {
-      val graphFile = args(0)
-      val nodes = parseGraph(graphFile)
-      nodes.foreach((pair) => {
-        println(pair._1 + " " + pair._2)
-      })
-    } else {
-      System.err.println("ERROR: Please enter a graph name")
-      System.exit(1)
-    }
-  }
-  private def parseGraph(fileName: String): Map[String, List[String]] = {
-    import scala.io.Source
-    val file = Source.fromFile(fileName)
-    var nodes = Map[String, List[String]]()
-    for(line <- file.getLines()) {
-      if (line.contains("->")) {
-        val edge = line
-          .replace("\"", "")
-          .split("->")
-          .map(_.trim)
-          .map((x:String) => {
-            if (x.contains(" ")) x.substring(0, x.indexOf(" ")) else x
-          })
-        val connectedNodesList = nodes.getOrElse(edge(0), List()) :+ edge(1)
-        nodes = nodes.updated(edge(0), connectedNodesList)
-      }
-    }
-    nodes
+    val graph: NetGraph = NetModelAlgebra().get
+    graph.persist("C:\\Users\\yortb\\Desktop\\cs441\\parseGraph\\graphs", NGSConstants.OUTPUTFILENAME)
   }
 }
